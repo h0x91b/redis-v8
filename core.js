aae0d7774261383e58e441c7efbf1d0db4068561
h0x91b <h0x91b@gmail.com>
2013-04-30 23:47:45 +0300

    [=] delete unused code from core & redis.hmget function

--- core.js
+++ core.js
@@ -1,66 +1,5 @@
-redis.str = {};//'';
-redis.bufindex = 0;
+redis.str = {};
 redis.last_error = '';
-
-redis.ok_return = function(){
-	redis.bufindex = redis.str.indexOf('\n',redis.bufindex)+1;
-	//str = redis.str.substr(redis.str.indexOf('\n')+1);
-	return true;
-}
-
-redis.err_return = function(){
-	//str = redis.str.substr(redis.str.indexOf('\n')+1);
-	var pos = redis.str.indexOf('\n',redis.bufindex)+1;
-	redis.last_error = redis.str.substr(redis.bufindex+1,pos-3);
-	redis.bufindex = pos;
-	return false;
-}
-
-redis.string_return = function(){
-	var pos = redis.str.indexOf('\n',redis.bufindex)+1;
-	var length = parseInt(redis.str.substr(redis.bufindex+1,pos-redis.bufindex-1));
-	//str = redis.str.substr(pos+length+2);
-	redis.bufindex = pos+length+2;
-	return redis.str.substr(pos,length);
-}
-
-redis.integer_return = function(){
-	var pos = redis.str.indexOf('\n',redis.bufindex)+1;
-	var ret = parseFloat(redis.str.substr(redis.bufindex+1,pos-redis.bufindex-1));
-	//str = redis.str.substr(redis.str.indexOf('\n')+1);
-	redis.bufindex = pos;
-	return ret;
-}
-
-redis.bulk_return = function(){
-	var obj = [];
-	var pos = redis.str.indexOf('\n',redis.bufindex)+1;
-	var bulks = parseInt(redis.str.substr(redis.bufindex+1,pos-redis.bufindex));
-	//str = redis.str.substr(pos);
-	redis.bufindex = pos;
-	for(var i=0;i<bulks;i++){
-		obj.push(redis.parse());
-	}
-	return obj;
-}
-
-redis.parse = function(){
-	if(redis.str[redis.bufindex]==='+'){
-		return redis.ok_return();
-	}
-	else if(redis.str[redis.bufindex]==='$') {
-		return redis.string_return();
-	}
-	else if(redis.str[redis.bufindex]===':'){
-		return redis.integer_return();
-	}
-	else if(redis.str[redis.bufindex]==='*'){
-		return redis.bulk_return();
-	}
-	else if(redis.str[redis.bufindex]==='-'){
-		return redis.err_return();
-	}
-}
 
 redis.run = function(){
 	redis.last_error = '';
@@ -68,7 +7,6 @@ redis.run = function(){
 	if(redis.str===false){
 		redis.last_error = redis.getLastError();
 	}
-	redis.bufindex = 0;
 	return redis.str;
 }
 
@@ -83,6 +21,27 @@ redis.exists = function(key){
 	return this.run('EXISTS',key);
 }
 
+redis.hmget = function(key,fields){
+	redis.last_error = '';
+	var args = fields;
+	if(Array.isArray(fields)){
+		args.unshift(key);
+		args.unshift('HMGET');
+	} else {
+		args = Array(arguments.length+1);
+		args[0] = 'HMGET';
+		for(var i=0;i<arguments.length;i++)
+			args[i+1] = arguments[i];
+	}
+	redis.str = redis.__run.apply(this,args);
+	
+	var resp = redis.str;
+	var ret = {};
+	for(var i=0;i<resp.length;i++)
+		ret[args[i+2]] = resp[i];
+	return ret;
+}
+
 redis.hmset = function(key, obj){
 	var f = ['HMSET',key];
 	for(var k in obj){
@@ -95,7 +54,6 @@ redis.hmset = function(key, obj){
 redis.hgetall = function(key){
 	redis.last_error = '';
 	redis.str = redis.__run.apply(this,['HGETALL',key]);
-	redis.bufindex = 0;
 	
 	if(redis.str.length<1) return null;
 	
