189e4228b67eddd899b859bf4c619fb319b13af0
h0x91b <h0x91b@gmail.com>
2013-05-03 15:27:30 +0300

    [=] rename redis.run into redis._run and some cleanup

--- redis/src/v8core.js
+++ redis/src/v8core.js
@@ -60,17 +60,17 @@ redis.v8stats = function(){
 }
 
 /* standart redis functions */
-// APPEND key value
+
 redis.append = function(key,value){
-	return redis.run('APPEND',key,value);
+	return this._run('APPEND',key,value);
 }
-// BGREWRITEAOF
+
 redis.bgrewriteaof = function(){
-	return redis.run('BGREWRITEAOF');
+	return this._run('BGREWRITEAOF');
 }
-// BGSAVE
+
 redis.bgsave = function(){
-	return redis.run('BGSAVE');
+	return this._run('BGSAVE');
 }
 // BITCOUNT key [start] [end]
 // BITOP operation destkey key [key ...]
@@ -78,40 +78,43 @@ redis.bgsave = function(){
 // BRPOP key [key ...] timeout
 // BRPOPLPUSH source destination timeout
 // CLIENT KILL ip:port
-// CLIENT LIST
+
 redis.client_list = function(list){
-	return this.run('CLIENT','LIST');
+	return this._run('CLIENT','LIST');
 }
 // CLIENT GETNAME
 // CLIENT SETNAME connection-name
 // CONFIG GET parameter
 // CONFIG SET parameter value
 // CONFIG RESETSTAT
-// DBSIZE
+
 redis.dbsize = function(){
-	return redis.run('DBSIZE');
+	return this._run('DBSIZE');
 }
-// BGSAVE
+
+redis.bgsave = function(){
+	return this._run('BGSAVE');
+}
 // DEBUG OBJECT key
 // DEBUG SEGFAULT
 // DECR key
 redis.decr = function(key) {
-	return this.run('DECR',key);
+	return this._run('DECR',key);
 }
-// DECRBY key decrement
+
 redis.decrby = function(key,decrement) {
-	return this.run('DECRBY',key,decrement);
+	return this._run('DECRBY',key,decrement);
 }
-// DEL key [key ...]
+
 redis.del = function(key){
 	if(arguments.length>1){
 		for(var i=0;i<arguments.length;i++)
-			this.run('DEL',arguments[i]);
+			this._run('DEL',arguments[i]);
 		return;
 	}
-	return this.run('DEL',key);
+	return this._run('DEL',key);
 }
-// DUMP key
+
 redis.dump = function(key){
 	redis.last_error = 'cant handle binary data, not implemented yet';
 	return false;
@@ -124,29 +127,32 @@ redis.dump = function(key){
 	})
 	return rez.join('');
 }
-// ECHO message
+
 redis.echo = function(message){
-	return redis.run('ECHO',message);
+	return this._run('ECHO',message);
 }
 // EVAL script numkeys key [key ...] arg [arg ...]
 // EVALSHA sha1 numkeys key [key ...] arg [arg ...]
-// EXISTS key
+
 redis.exists = function(key){
-	return this.run('EXISTS',key);
+	return this._run('EXISTS',key);
 }
-// EXPIRE key seconds
+
+redis.expire = function(key,seconds){
+	return this._run('EXPIRE',key,seconds);
+}
 // EXPIREAT key timestamp
-// FLUSHALL
+
 redis.flushall = function(){
-	return redis.run('FLUSHALL');
+	return this._run('FLUSHALL');
 }
-// FLUSHDB
+
 redis.flushdb = function(){
-	return redis.run('FLUSHDB');
+	return this._run('FLUSHDB');
 }
-// GET key
+
 redis.get = function(key){
-	return this.run('GET',key);
+	return this._run('GET',key);
 }
 // GETBIT key offset
 // GETRANGE key start end
@@ -154,7 +160,7 @@ redis.get = function(key){
 // HDEL key field [field ...]
 // HEXISTS key field
 // HGET key field
-// HGETALL key
+
 redis.hgetall = function(key){
 	redis._runcounter++;
 	redis.last_error = '';
@@ -171,13 +177,13 @@ redis.hgetall = function(key){
 }
 // HINCRBY key field increment
 // HINCRBYFLOAT key field increment
-// HKEYS key
+
 redis.hkeys = function(key){
-	return this.run('HKEYS',key);
+	return this._run('HKEYS',key);
 }
-// HLEN key
+
 redis.hlen = function(key){
-	return this.run('HLEN',key);
+	return this._run('HLEN',key);
 }
 // HMGET key field [field ...]
 redis.hmget = function(key,fields){
@@ -201,49 +207,49 @@ redis.hmget = function(key,fields){
 		ret[args[i+2]] = resp[i];
 	return ret;
 }
-// HMSET key field value [field value ...]
+
 redis.hmset = function(key, obj){
 	var f = ['HMSET',key];
 	for(var k in obj){
 		f.push(k);
 		f.push(obj[k]);
 	}
-	return this.run.apply(this,f);
+	return this._run.apply(this,f);
 }
 // HSET key field value
 // HSETNX key field value
-// HVALS key
+
 redis.hvals = function(key){
-	return this.run('HVALS',key);
+	return this._run('HVALS',key);
 }
-// INCR key
+
 redis.incr = function(key){
-	return this.run('INCR',key);
+	return this._run('INCR',key);
 }
 // INCRBY key increment
 // INCRBYFLOAT key increment
 // INFO [section]
-// KEYS pattern
+
 redis.keys = function(pattern){
-	return this.run('KEYS',pattern);
+	return this._run('KEYS',pattern);
 }
-// LASTSAVE
+
 redis.lastsave = function(){
-	return redis.run('LASTSAVE');
+	return this._run('LASTSAVE');
 }
 // LINDEX key index
 // LINSERT key BEFORE|AFTER pivot value
-// LLEN key
+
 redis.llen = function(key){
-	return this.run('LLEN',key);
+	return this._run('LLEN',key);
 }
-// LPOP key
+
 redis.lpop = function(key){
-	return this.run('LPOP',key);
+	return this._run('LPOP',key);
 }
 // LPUSH key value [value ...]
 redis.lpush = function(key,value){
-	return this.run('LPUSH',key,value);
+	return this._run('LPUSH',key,value);
 }
 // LPUSHX key value
 // LRANGE key start stop
@@ -256,43 +262,49 @@ redis.lpush = function(key,value){
 // MSET key value [key value ...]
 // MSETNX key value [key value ...]
 // OBJECT subcommand [arguments [arguments ...]]
-// PERSIST key
+
 redis.persist = function(key){
-	return this.run('PERSIST',key);
+	return this._run('PERSIST',key);
 }
 // PEXPIRE key milliseconds
 // PEXPIREAT key milliseconds-timestamp
-// PING
+
 redis.ping = function(){
-	return this.run('PING');
+	return this._run('PING');
 }
 // PSETEX key milliseconds value
-// PTTL key
+
 redis.pttl = function(key){
-	return this.run('PTTL',key);
+	return this._run('PTTL',key);
 }
-// RANDOMKEY
+
 redis.randomkey = function(){
-	return redis.run('RANDOMKEY');
+	return this._run('RANDOMKEY');
 }
-// RENAME key newkey
-// RENAMENX key newkey
+
+redis.rename = function(key,newkey){
+	return this._run('RENAME',key,newkey);
+}
+
+redis.renamenx = function(key,newkey){
+	return this._run('RENAMENX',key,newkey);
+}
 // RESTORE key ttl serialized-value
-// RPOP key
+
 redis.rpop = function(key){
-	return this.run('RPOP',key);
+	return this._run('RPOP',key);
 }
 // RPOPLPUSH source destination
 // RPUSH key value [value ...]
 // RPUSHX key value
 // SADD key member [member ...]
-// SAVE
+
 redis.save = function(){
-	return redis.run('SAVE');
+	return this._run('SAVE');
 }
-// SCARD key
+
 redis.scard = function(key){
-	return this.run('SCARD',key);
+	return this._run('SCARD',key);
 }
 // SCRIPT EXISTS script [script ...]
 // SCRIPT FLUSH
@@ -300,9 +312,9 @@ redis.scard = function(key){
 // SCRIPT LOAD script
 // SDIFF key [key ...]
 // SDIFFSTORE destination key [key ...]
-// SELECT index
+
 redis.select = function(index){
-	return this.run('SELECT',index);
+	return this._run('SELECT',index);
 }
 // SET key value [EX seconds] [PX milliseconds] [NX|XX]
 redis.set = function(key,value){
@@ -312,15 +324,15 @@ redis.set = function(key,value){
 		args[0] = 'SET';
 		for(var i=0;i<arguments.length;i++)
 			args[i+1] = arguments[i];
-		return this.run.apply(this,args);
+		return this._run.apply(this,args);
 	}
-	return this.run('SET',key,value);
+	return this._run('SET',key,value);
 }
 
 // SETBIT key offset value
 // SETEX key seconds value
 redis.setex = function(key,expire,value){
-	return this.run('SETEX',key,expire,value);
+	return this._run('SETEX',key,expire,value);
 }
 // SETNX key value
 // SETRANGE key offset value
@@ -332,40 +344,40 @@ redis.setex = function(key,expire,value){
 // SLOWLOG subcommand [argument]
 // SMEMBERS key
 redis.smembers = function(key){
-	return this.run('SMEMBERS',key);
+	return this._run('SMEMBERS',key);
 }
 // SMOVE source destination member
 // SORT key [BY pattern] [LIMIT offset count] [GET pattern [GET pattern ...]] [ASC|DESC] [ALPHA] [STORE destination]
-// SPOP key
+
 redis.spop = function(key){
-	return this.run('SPOP',key);
+	return this._run('SPOP',key);
 }
 // SRANDMEMBER key [count]
 // SREM key member [member ...]
-// STRLEN key
+
 redis.strlen = function(key){
-	return this.run('STRLEN',key);
+	return this._run('STRLEN',key);
 }
 // SUBSCRIBE channel [channel ...]
 // SUNION key [key ...]
 // SUNIONSTORE destination key [key ...]
-// SYNC
+
 redis.sync = function(){
-	return redis.run('SYNC');
+	return this._run('SYNC');
 }
-// TIME
+
 redis.time = function(){
-	return redis.run('TIME');
+	return this._run('TIME');
 }
-// TTL key
+
 redis.ttl = function(key){
-	return this.run('TTL',key);
+	return this._run('TTL',key);
 }
-// TYPE key
+
 redis.type = function(key){
-	return this.run('TYPE',key);
+	return this._run('TYPE',key);
 }
-// ZADD key score member [score member ...]
+
 redis.zadd = function(key,score,value){
 	if(arguments.length>2){
 		var args = [];
@@ -373,13 +385,13 @@ redis.zadd = function(key,score,value){
 		args[0] = 'ZADD';
 		for(var i=0;i<arguments.length;i++)
 			args[i+1] = arguments[i];
-		return this.run.apply(this,args);
+		return this._run.apply(this,args);
 	}
-	return this.run('ZADD',key,score,value);
+	return this._run('ZADD',key,score,value);
 }
-// ZCARD key
+
 redis.zcard = function(key){
-	return this.run('ZCARD',key);
+	return this._run('ZCARD',key);
 }
 // ZCOUNT key min max
 // ZINCRBY key increment member
@@ -387,12 +399,12 @@ redis.zcard = function(key){
 // ZRANGE key start stop [WITHSCORES]
 redis.zrange = function(key,start,stop){
 	if(typeof withscores != 'undefined')
-		return this.run('ZRANGE',key,start,stop,'WITHSCORES');
-	return this.run('ZRANGE',key,start,stop);
+		return this._run('ZRANGE',key,start,stop,'WITHSCORES');
+	return this._run('ZRANGE',key,start,stop);
 }
 // ZRANGEBYSCORE key min max [WITHSCORES] [LIMIT offset count]
 // ZRANK key member
-// ZREM key member [member ...]
+
 redis.zrem = function(key,value){
 	if(arguments.length>2){
 		var args = [];
@@ -400,9 +412,9 @@ redis.zrem = function(key,value){
 		args[0] = 'ZREM';
 		for(var i=0;i<arguments.length;i++)
 			args[i+1] = arguments[i];
-		return this.run.apply(this,args);
+		return this._run.apply(this,args);
 	}
-	return this.run('ZREM',key,value);
+	return this._run('ZREM',key,value);
 }
 
 // ZREMRANGEBYRANK key start stop
@@ -410,8 +422,8 @@ redis.zrem = function(key,value){
 // ZREVRANGE key start stop [WITHSCORES]
 redis.zrevrange = function(key,start,stop,withscores){
 	if(typeof withscores != 'undefined')
-		return this.run('ZREVRANGE',key,start,stop,'WITHSCORES');
-	return this.run('ZREVRANGE',key,start,stop);
+		return this._run('ZREVRANGE',key,start,stop,'WITHSCORES');
+	return this._run('ZREVRANGE',key,start,stop);
 }
 // ZREVRANGEBYSCORE key max min [WITHSCORES] [LIMIT offset count]
 // ZREVRANK key member
