53e5620ba8b48d9bf473bb66ccdb798eb615b933
h0x91b <h0x91b@gmail.com>
2013-06-18 01:07:11 +0300

    [+] Model testing

---
+++ js/Model-test.js
@@ -0,0 +1,43 @@
+var author = Model('author',{
+	name: 'h0x91B',
+	carma: 100
+})
+
+for(var i=0;i<50;i++){
+	var post = Model('post', {
+		author: author.id,
+		title: 'some title',
+		body: 'some body',
+		rating: ~~(Math.random()*1000)
+	});
+	for(var n=0; n<50; n++){
+		Model('post:'+post.id+':comment',{
+			title: 'comment title',
+			body: 'comment body',
+			author: author.id
+		})
+	}
+}
+
+console.log('model test',
+	Model('post')
+		.head(100)
+		.where('id',function(id){
+			return id % 2;
+		})
+		.where('id',function(id){
+			return id > 50;
+		})
+		.getAll()
+		.orderBy('rating','DESC')
+		.limit(10)
+		.each(function(post){
+			post.author = Model('author').get(post.author);
+			post.comments = Model('post:'+post.id+':comment')
+								.head(3)
+								.getAll()
+								.each(function(comment){
+									comment.author = Model('author').get(comment.author);
+								})
+		})
+);
