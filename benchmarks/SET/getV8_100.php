849abed2c2c1129121fb98b7b98f50094049b5cb
h0x91b <h0x91b@gmail.com>
2013-07-04 15:24:43 +0300

    [+] SET benchs

---
+++ benchmarks/SET/getV8_100.php
@@ -0,0 +1,15 @@
+<?php
+date_default_timezone_set('Europe/Moscow');
+require '../../Client-Libraries/PHP/predis-0.8.3/Autoloader.php';
+Predis\Autoloader::register();
+
+$redis = new Predis\Client('unix:///tmp/redis.sock');
+
+$start = microtime(TRUE);
+for($i=0;$i<1000000/100;$i++){
+	$redis->js('var ret = []; for(var i=0;i<100;i++) ret.push(redis.set("bench_key", "value")); return ret;');
+}
+echo "Speed: ".(round(1000000/(microtime(TRUE)-$start)))." ops/sec\n";
+$redis->del('bench_key');
+//total commands 157501
+?>
---
+++ benchmarks/SET/get_pipe_500.php
@@ -0,0 +1,17 @@
+<?php
+date_default_timezone_set('Europe/Moscow');
+require '../../Client-Libraries/PHP/predis-0.8.3/Autoloader.php';
+Predis\Autoloader::register();
+
+$redis = new Predis\Client('unix:///tmp/redis.sock');
+
+$start = microtime(TRUE);
+for($i=0;$i<100000/500;$i++){
+	$pipe = $redis->pipeline();
+	for($n=0;$n<500;$n++)
+		$pipe->set('bench_key','value');
+	$pipe->execute();
+}
+echo "Speed: ".(round(100000/(microtime(TRUE)-$start)))." ops/sec\n";
+$redis->del('bench_key');
+?>
