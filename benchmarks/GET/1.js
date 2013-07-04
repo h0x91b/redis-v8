d6b80f81d2d43ef6fc9036ce7b417b184fb33307
h0x91b <h0x91b@gmail.com>
2013-07-04 16:23:25 +0300

    [=]

--- benchmarks/GET/1.js
+++ benchmarks/GET/1.js
@@ -5,15 +5,18 @@ client.on("error", function (err) {
 	console.log("Error " + err);
 });
 
-var start = +new Date;
-var done = 0;
-for(var i=0;i<100000;i++){
-	client.get('bench_key',function(err, reply){
-		done++;
-		if(done==100000){
-			var dt = new Date - start;
-			console.log('Speed: '+Math.round(100000/(dt/1000))+' ops/sec')
-			client.quit();
-		}
-	});
-}
+client.set('bench_key', 'value', function(err,reply){
+	var start = +new Date;
+	var done = 0;
+	
+	for(var i=0;i<100000;i++){
+		client.get('bench_key',function(err, reply){
+			done++;
+			if(done==100000){
+				var dt = new Date - start;
+				console.log('Speed: '+Math.round(100000/(dt/1000))+' ops/sec')
+				client.quit();
+			}
+		});
+	}
+});
